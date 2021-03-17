0a20769bc60fb57ca92d8ef39a8e83fdc1ff674a
Steven Diaz <stevenbdf@gmail.com>
2021-03-17 00:05:39 -0600

    ✨ Create files files

---
+++ database/migrations/2021_03_17_000431_create_media_files_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('media_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('file_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media_files');
+    }
+}
